cc7be537eaa2ebf890caab826266ca4a7bbfc109
Okoi IBIANG <oibiang@nse.com.ng>
2022-04-28 13:45:18 +0100

    First commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Elev8NetCoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elev8NetCoreApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        public IActionResult List()
+        {
+           EmployeeDetailsInfo edc = new EmployeeDetailsInfo();
+           DataTable dt= edc.ReturnEmployeesRecord();
+
+           List<EmployeeDetailsInfo> edcList = new List<EmployeeDetailsInfo>();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                EmployeeDetailsInfo ed = new EmployeeDetailsInfo();
+                ed.Id =Convert.ToInt32( dr["Id"]);
+                ed.surName = dr["surName"].ToString();
+                ed.otherNames = dr["otherNames"].ToString();
+                ed.mobileNo = dr["mobileNo"].ToString();
+                ed.gender = dr["gender"].ToString();
+                ed.address = dr["address"].ToString();
+
+                edcList.Add(ed);
+
+            }
+
+           return View(edcList);
+        }
+
+        public IActionResult Create(EmployeeDetailsInfo ed)
+        {
+            if (!String.IsNullOrEmpty(ed.surName))
+            {
+                EmployeeDetailsInfo edic = new EmployeeDetailsInfo();
+                int result = edic.CreateEmployeeRecord(ed);
+                if (result > 0)
+                {
+                    ViewBag.result = "Record created successfully.";
+                }
+                else
+                {
+                    ViewBag.result = "Error creating record";
+                }
+            }
+           
+            return View();
+        }
+
+        public IActionResult Details(int Id)
+        {
+            EmployeeDetailsInfo edc = new EmployeeDetailsInfo();
+            DataTable dt = edc.ReturnEmployeesRecord(Id);
+
+            EmployeeDetailsInfo ed = new EmployeeDetailsInfo();
+            foreach (DataRow dr in dt.Rows)
+            {
+                 
+                ed.Id = Convert.ToInt32(dr["Id"]);
+                ed.surName = dr["surName"].ToString();
+                ed.otherNames = dr["otherNames"].ToString();
+                ed.mobileNo = dr["mobileNo"].ToString();
+                ed.gender = dr["gender"].ToString();
+                ed.address = dr["address"].ToString();
+
+            }
+
+            return View(ed);
+        }
+
+
+    }
+}
---
+++ Models/EmployeeDetailsInfo.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elev8NetCoreApp.Models
+{
+    public class EmployeeDetailsInfo
+    {
+        public int Id { get; set; }
+        [Display(Name="Surname:")]
+        [Required(ErrorMessage ="Surname is Required.")]
+        public string surName { get; set; }
+        [Display(Name = "Other Names:")]
+
+        
+        public string otherNames { get; set; }
+        [Display(Name = "Mobile No:")]
+        [Required(ErrorMessage = "Mobile is Required.")]
+        public string mobileNo { get; set; }
+        [Display(Name = "Address:")]
+        public string address { get; set; }
+        [Display(Name = "Gender:")]
+        public string gender { get; set; }
+
+        public int CreateEmployeeRecord(EmployeeDetailsInfo employeesInfo)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(Startup.connectionString))
+                {
+                    //Insert into [TableName](Properties) Values(content)
+                    String query = "INSERT INTO EmployeesDetailsTbl(Surname,OtherNames,Gender, MobileNo,Address) Values(@Surname,@OtherNames,@Gender, @MobileNo,@Address)";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Surname", employeesInfo.surName);
+                        command.Parameters.AddWithValue("@OtherNames", employeesInfo.otherNames);
+                        command.Parameters.AddWithValue("@Gender", employeesInfo.gender);
+                        command.Parameters.AddWithValue("@MobileNo", employeesInfo.mobileNo);
+                        command.Parameters.AddWithValue("@Address", employeesInfo.address);
+
+                        connection.Open();
+                        int result = command.ExecuteNonQuery();
+
+                        return result;
+
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+                return -1;
+            }
+
+        }
+
+        public DataTable ReturnEmployeesRecord()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection connection = new SqlConnection(Startup.connectionString))
+            {
+                String sql = "SELECT * FROM EmployeesDetailsTbl order by Id desc";
+
+                SqlCommand sqlcom = new SqlCommand(sql, connection);
+                try
+                {
+                    sqlcom.Connection.Open();
+                    sqlcom.ExecuteNonQuery();
+                    SqlDataReader reader = sqlcom.ExecuteReader();
+
+                    dt.Load(reader);
+                    
+                }
+                catch (SqlException ex)
+                {
+                    // MessageBox.Show(ex.Message);
+                }
+            }
+
+
+            return dt;
+        }
+
+        public DataTable ReturnEmployeesRecord(int Id)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection connection = new SqlConnection(Startup.connectionString ))
+            {
+                String sql = "SELECT * FROM EmployeesDetailsTbl where Id=@Id";
+                using (SqlCommand sqlcom = new SqlCommand(sql, connection))
+                {
+
+                    try
+                    {
+                        sqlcom.Parameters.AddWithValue("@Id", Id);
+                        sqlcom.Connection.Open();
+                        sqlcom.ExecuteNonQuery();
+                        SqlDataReader reader = sqlcom.ExecuteReader();
+
+                        dt.Load(reader);
+
+                    }
+                    catch (SqlException ex)
+                    {
+                        //MessageBox.Show(ex.Message);
+                    }
+
+                }
+
+
+            }
+
+
+            return dt;
+        }
+
+    }
+}
